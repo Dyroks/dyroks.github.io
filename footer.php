8c6db24e1c2eb68eaf6e2e400368d092f356da39
Dyroks <dyroks@gmail.com>
2016-08-10 02:43:14 +0200

    TG

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <a href="contact.php">Me contacter</a>
-</footer>
--- head.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="style.css" />
-<link rel="icon" type="image/png" href="favicon.png" sizes="16x16" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<meta charset="utf-8" />
